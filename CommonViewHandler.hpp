3042ec115ce7ace0594e8e765eed05ce8cce804d
ediston <vivek4dhiman@gmail.com>
2020-06-14 13:40:13 +0530

    Create CommonViewHandler.hpp

---
+++ CommonViewHandler.hpp
@@ -0,0 +1,21 @@
+//
+//  NewHome
+//
+//  Created by VivekDhiman on 02/01/14.
+//
+//
+#ifndef __CommonViewHandler__
+#define __CommonViewHandler__
+
+#include "common.h"
+USING_NS_CC;
+using namespace std;
+
+class CommonViewHandler {
+public:
+    CommonViewHandler();
+    static void loadGoogleAds();
+    static void showBottomGoogleAd();
+    static void hideBottomGoogleAd();
+};
+#endif
