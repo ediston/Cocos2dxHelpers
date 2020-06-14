0dfcace77beb006dc87d93212ed4d11965ee59d3
ediston <vivek4dhiman@gmail.com>
2020-06-14 13:38:58 +0530

    Create ObjCCalls.h

---
+++ ObjCCalls.h
@@ -0,0 +1,13 @@
+#ifndef __ObjCCalls_H__
+#define __ObjCCalls_H__
+
+#include <stddef.h>
+class ObjCCalls
+private: 
+public:
+    static void loadGoogleAds();
+    static void showBottomGoogleAd();
+    static void hideBottomGoogleAd();
+};
+
+#endif
